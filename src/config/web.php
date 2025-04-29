a759d2b00fa23c20d893d5ae1981d23a4656e083
tohidhanfi20 <tohidhanfi20@gmail.com>
2025-04-29 22:08:03 +0530

    updated ansible files

---
+++ src/config/web.php
@@ -0,0 +1,41 @@
+<?php
+
+$config = [
+    'id' => 'basic',
+    'basePath' => dirname(__DIR__),
+    'bootstrap' => ['log'],
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'your-secret-key-here',
+        ],
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'user' => [
+            'identityClass' => 'app\models\User',
+            'enableAutoLogin' => true,
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'useFileTransport' => true,
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        // Comment out DB if you don't have db.php
+        // 'db' => require __DIR__ . '/db.php',
+    ],
+    'params' => [],
+];
+
+return $config;
