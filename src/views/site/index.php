a4b59b36786ce5704ce588d2aab4c11a2c5f9f7f
tohidhanfi20 <tohidhanfi20@gmail.com>
2025-04-29 22:15:04 +0530

    updated ansible files

--- src/views/site/index.php
+++ src/views/site/index.php
@@ -1,2 +1,9 @@
 <?php
-echo "Hello, Yii2!";
+$this->title = 'My Yii Application';
+?>
+<div class="site-index">
+    <div class="jumbotron">
+        <h1>Congratulations!</h1>
+        <p class="lead">You have successfully created your Yii-powered application.</p>
+    </div>
+</div>
